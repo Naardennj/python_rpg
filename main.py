f3f00a4fe8d55613bb8e93fe25d74b09c3aaf852
Naardennj <gabriel.angelis787@gmail.com>
2025-07-14 10:04:28 -0300

    Função criar player

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ from random import randint
 
 npcs_list = [] 
 
+player = {
+    "name": "Gabriel",
+    "level": 1,
+    "exp": 0,
+    "exp_max": 50,
+    "hp": 100,
+    "hp_max": 100,
+    "damage": 25,
+
+}
+
 def create_monster():
     level = randint(0, 50)
     
     new_npc = {
-        "Name" : f"Monster #{level}",
-        "Level": level,
-        "Damage": 5 * level,
-        "Health": 100 * level,
+        "name" : f"Monster #{level}",
+        "level": level,
+        "damage": 5 * level,
+        "health": 100 * level,
+        "exp": 7 * level,
     }
 
     return new_npc
@@ -21,7 +33,7 @@ def create_monsters (n_npcs):
 
 def show_npcs():
     for npc in npcs_list:
-        print(f"Name: {npc['Name']} // Level: {npc['Level']} // Damage: {npc['Damage']} // Health: {npc['Health']}")
+        print(f"Name: {npc['name']} // Level: {npc['level']} // Damage: {npc['damage']} // Health: {npc['health']}")
 
 create_monsters(5)
 show_npcs()
